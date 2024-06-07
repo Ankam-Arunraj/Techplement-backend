428b0df01b7991fcf3db23a138c17dc8f524ddb3
ANKAM ARUNRAJ <your_20R01A04J3@cmritonline.ac.in>
2024-06-07 15:45:47 +0530

    backend tsl change

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const quoteRoutes = require("./routes/quote");
 
- mongoose.connect("mongodb+srv://Arunraj29:arun2901@arunraj.xne4ztq.mongodb.net/?retryWrites=true&w=majority&appName=arunraj").then(() => {
+
+//  mongoose.connect("mongodb+srv://Arunraj29:arun2901@arunraj.xne4ztq.mongodb.net/?retryWrites=true&w=majority&appName=arunraj").then(() => {
+//     console.log("Connected to MongoDB");
+//  })
+
+mongoose.connect(process.env.MONGODB_URI, {
+    // useNewUrlParser: true,
+    // useUnifiedTopology: true,
+    tls: true,
+    // ssl: true,
+    // sslValidate: false, // only if you're using self-signed certificates
+}).then(() => {
     console.log("Connected to MongoDB");
- })
+}).catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
 
 const app = express();
 app.use(express.json())
